f1b0e0494bbc1302dfe0c7e1c2776170bdfc140d
Flavio Poletti <flavio.poletti@hotmail.ch>
2023-03-05 22:16:56 +0100

    revert some "players" typos

--- schieber/players/model_player.py
+++ schieber/players/model_player.py
@@ -13,13 +13,13 @@ logger = logging.getLogger(__name__)
 
 class ModelPlayer(GreedyPlayer):
     """
-    This players can be used to evaluate the strength of a trained RL model.
+    This player can be used to evaluate the strength of a trained RL model.
     """
 
     def __init__(self, name='unknown', seed=None, trumps='all',
                  model_path="/Users/joelito/MEGA/Studium/Master/Informatik/Courses/Data Science/Very Deep Learning/Project/schieber/tests/benchmarks/models/stich-higher-learning-rate_env=Schieber-v0_gamma=0.89_nsteps=90_learning_rate=0.001_policy=JassPolicy_model=PPO2_time=2019-01-13_12:08:28_final.pkl"):
         """
-        Inits the players with a trained model. This model is based on the stable_baselines framework.
+        Inits the player with a trained model. This model is based on the stable_baselines framework.
         (It has to implement the predict() function)
         :param model_path:
         """
